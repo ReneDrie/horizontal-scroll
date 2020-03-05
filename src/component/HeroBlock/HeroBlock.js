be369d087fd3c614d2353835da70fca9f8e1fc73
René <rene@mediamonks.com>
2020-03-05 09:02:42 +0100

    Add some Parallax for the Video

--- src/component/HeroBlock/HeroBlock.js
+++ src/component/HeroBlock/HeroBlock.js
@@ -15,9 +15,15 @@ export default {
     },
 
     onViewUpdate(xOffset) {
+      // We make the "Title" and the "Background" sticky with code here
       const max = this.$el.offsetWidth - this.$refs.background.offsetWidth;
-      TweenLite.set([this.$refs.background, this.$refs.title], {
+      TweenLite.set(this.$refs.title, {
         x: Math.min(max, Math.max(0, xOffset)),
+      });
+
+      // Added some parralax at the end
+      TweenLite.set(this.$refs.background, {
+        x: xOffset < max ? xOffset : max + (xOffset - max) / 2,
       });
 
       // Ugly calculation to start later and speed up the text animation
